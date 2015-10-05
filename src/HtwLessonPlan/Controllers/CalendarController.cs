3798f3861f7db398c97e2b7b87a6506ffd5d6e38
Oliver Guhr <oliver.guhr@gmx.de>
2015-10-05 20:57:07 +0200

    api endpoint added

--- src/HtwLessonPlan/Controllers/CalendarController.cs
+++ src/HtwLessonPlan/Controllers/CalendarController.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using HtwLessonPlan.Model;
+using Microsoft.Net.Http.Headers;
+using System.Net;
 
 namespace HtwLessonPlan.Controllers
 {
     [Route("api/[controller]")]
     public class CalendarController : Controller
     {
-        // GET api/calendar/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/calendar/12345  
+        [Produces("text/calendar")]
+        [HttpGet("{studentNumber}")]
+        public async Task<IActionResult> Get(string studentNumber)
         {
-            return "value";
+            HtwWebservice htw = new HtwWebservice(new Uri("http://www2.htw-dresden.de/~rawa/cgi-bin/auf/raiplan_kal.php"));
+
+            List<CalendarEvent> calendar = new List<CalendarEvent>();
+
+            try
+            {
+                calendar = await htw.LoadCalendar(studentNumber);
+            }
+            catch (Exception)
+            {
+                
+                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+            var data = System.Text.Encoding.UTF8.GetBytes(Ical.Generate(calendar));
+            return new FileContentResult(data, "text/calendar");
         }
     }
 }
