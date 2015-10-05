4c6f46d9a8861b29ff023c871814504497374c79
Oliver Guhr <oliver.guhr@gmx.de>
2015-10-05 19:23:29 +0200

    api cleanup

--- src/HtwLessonPlan/Controllers/ValuesController.cs
+++ src/HtwLessonPlan/Controllers/ValuesController.cs
@@ -7,38 +7,13 @@ using Microsoft.AspNet.Mvc;
 namespace HtwLessonPlan.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class CalendarController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
+        // GET api/Calendar/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
